ee2e0325e211bc75d190f9c2606402deee00cccd
KYapp <kukiyomenaisapp@gmail.com>
2022-12-21 19:27:48 +0900

    CreateThreadを関数に分離

--- shigLeCodeGolfBot/Program.cs
+++ shigLeCodeGolfBot/Program.cs
@@ -1,11 +1,62 @@
 ﻿using paizaIOSharp;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
 
 namespace shigLeCodeGolfBot;
 
 class Program
 {
-    static void Main(string[] args)
+    public async Task MainAsync()
     {
-        Console.WriteLine("Hello, World!");
+        var client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.All
+        });
+        client.Log += LogAsync;
+        client.Ready += OnReady;
+        client.MessageReceived += OnMessage;
+        await client.LoginAsync(TokenType.Bot, "MTA1NTAwMzM0NDMxOTU0OTQ0MA.G8k9Ns.xUBexKQuB58sDwVQsrinFi7QKyVRE6McI151VM");
+        await client.StartAsync();
+        await Task.Delay(Timeout.Infinite);
     }
+
+    private Task OnReady()
+    {
+        Console.WriteLine("OnReady");
+        return Task.CompletedTask;
+    }
+
+    private Task OnMessage(SocketMessage message)
+    {
+        Console.WriteLine("{0} {1}:{2}", message.Channel.Name, message.Author.Username, message);
+
+        // botの場合すぐ返す
+        if (message.Author.IsBot) return Task.CompletedTask;
+
+        // テストで返す
+        message.Channel.SendMessageAsync("てぇぇぇすとぉぉぉでぇすねぇぇぇぇええええ！！！！");
+        CreateThread("testThread", message.Channel as ITextChannel);
+
+        return Task.CompletedTask;
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private Task CreateThread(string name,ITextChannel? channel){
+        channel?.CreateThreadAsync(
+            name: name,
+            autoArchiveDuration: ThreadArchiveDuration.OneWeek,
+            invitable: false,
+            type: ThreadType.PublicThread
+        ).Wait();
+
+        return Task.CompletedTask;
+    }
+
+    public static void Main(string[] args) => new Program().MainAsync().Wait();
 }
