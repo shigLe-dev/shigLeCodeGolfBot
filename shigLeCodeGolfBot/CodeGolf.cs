e39c5d58af1f57444cd3d782d70620070f667065
KYapp <kukiyomenaisapp@gmail.com>
2022-12-21 20:19:02 +0900

    SlashCommandに対応した

---
+++ shigLeCodeGolfBot/CodeGolf.cs
@@ -0,0 +1,11 @@
+namespace shigLeCodeGolfBot;
+
+public class CodeGolf
+{
+    public readonly ulong threadId;
+
+    public CodeGolf(ulong threadId)
+    {
+        this.threadId = threadId;
+    }
+}
--- shigLeCodeGolfBot/Program.cs
+++ shigLeCodeGolfBot/Program.cs
@@ -7,36 +7,53 @@ namespace shigLeCodeGolfBot;
 
 class Program
 {
+    DiscordSocketClient client;
+
     public async Task MainAsync()
     {
-        var client = new DiscordSocketClient(new DiscordSocketConfig
+        client = new DiscordSocketClient(new DiscordSocketConfig
         {
             GatewayIntents = GatewayIntents.All
         });
         client.Log += LogAsync;
         client.Ready += OnReady;
         client.MessageReceived += OnMessage;
+        client.SlashCommandExecuted += OnSlashCommand;
         await client.LoginAsync(TokenType.Bot, ConfigurationManager.AppSettings["token"]);
         await client.StartAsync();
         await Task.Delay(Timeout.Infinite);
     }
 
-    private Task OnReady()
+    private async Task OnReady()
     {
         Console.WriteLine("OnReady");
-        return Task.CompletedTask;
+
+        await SetCommand();
+    }
+
+    private async Task SetCommand(){
+        var guild = client.GetGuild(1031157559404548107);
+
+        var guildCommand = new SlashCommandBuilder();
+        guildCommand.WithName("codegolf");
+        guildCommand.WithDescription("このコマンドを使用することで、新たにCodeGolfを始めることが出来ます。");
+
+        try
+        {
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
     private Task OnMessage(SocketMessage message)
     {
         Console.WriteLine("{0} {1}:{2}", message.Channel.Name, message.Author.Username, message);
-
+        
         // botの場合すぐ返す
         if (message.Author.IsBot) return Task.CompletedTask;
-
-        // テストで返す
-        message.Channel.SendMessageAsync("てぇぇぇすとぉぉぉでぇすねぇぇぇぇええええ！！！！");
-        CreateThread("testThread", message.Channel as ITextChannel);
 
         return Task.CompletedTask;
     }
@@ -51,12 +68,17 @@ class Program
     {
         channel?.CreateThreadAsync(
             name: name,
-            autoArchiveDuration: ThreadArchiveDuration.OneWeek,
+            autoArchiveDuration: ThreadArchiveDuration.OneDay,
             invitable: false,
             type: ThreadType.PublicThread
         ).Wait();
 
         return Task.CompletedTask;
+    }
+
+    private async Task OnSlashCommand(SocketSlashCommand command){
+        await CreateThread("test", command.Channel as ITextChannel);
+        await command.RespondAsync("スレッド作った");
     }
 
     public static void Main(string[] args) => new Program().MainAsync().Wait();
